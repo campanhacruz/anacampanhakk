82349faafe8d07ebb8acba6a67b80a2e58061e87
campanhacruz <167535130+campanhacruz@users.noreply.github.com>
2024-11-01 10:39:15 -0300

    Add files via upload

---
+++ Ana CLara/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Algoritmo',
+    'O que é um algoritmo?',
+    'O que é lógica de programação?'
+)
+
+criaCartao(
+    'função',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua Espalhola',
+    'Como se diz oi em espanhol?',
+    'Oi em espanhol é hola '
+)
